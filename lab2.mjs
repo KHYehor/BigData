2de4a21fafddb7e5da00404c855efcaf24b2909a
KHYehor <khilchenko.yehor@gmail.com>
2021-11-23 23:50:17 +0200

    feat: add promisify to exec

--- lab2.mjs
+++ lab2.mjs
@@ -1,6 +1,7 @@
 import fetch from 'node-fetch';
 import fs from 'fs';
-import { exec } from 'child_process';
+import { exec as execCallback } from 'child_process';
+import util from 'util';
 
 const LINK_TO_PULL = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-20/master/data_tables/vaccine_data/global_data/time_series_covid19_vaccine_global.csv';
 const CSV_DATA_FILE = './vaccines.csv';
@@ -9,6 +10,7 @@ const HADOOP_PUT_DOC = 'hadoop fs -put ./covid19/vaccines.csv /covid19/vaccines'
 const TIMER_LABEL = 'Done in';
 
 const WriteStream = fs.createWriteStream(CSV_DATA_FILE);
+const exec = util.promisify(execCallback);
 
 const pullData = () => new Promise((resolve, reject) => {
     fetch(LINK_TO_PULL)
