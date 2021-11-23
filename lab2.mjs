d9fc0b5bddbe1901a3180ef528bdb47c8da7a5f4
KHYehor <khilchenko.yehor@gmail.com>
2021-11-23 23:41:45 +0200

    feat: add ETL script

---
+++ lab2.mjs
@@ -0,0 +1,32 @@
+import fetch from 'node-fetch';
+import fs from 'fs';
+import { exec } from 'child_process';
+
+const LINK_TO_PULL = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-20/master/data_tables/vaccine_data/global_data/time_series_covid19_vaccine_global.csv';
+const CSV_DATA_FILE = './covid19/vaccines.csv';
+const HADOOP_CREATE_DIR = 'hadoop fs -mkdir /covid19/vaccines';
+const HADOOP_PUT_DOC = 'hadoop fs -put ./covid19/vaccines.csv /covid19/vaccines';
+
+const WriteStream = fs.createWriteStream(CSV_DATA_FILE);
+
+const pullData = () => new Promise((resolve, reject) => {
+    fetch(LINK_TO_PULL)
+        .then(res => {
+            res.body.pipe(WriteStream)
+                .on('finish', () => resolve())
+                .on('error', err => reject(err));
+        })
+        .catch(err => reject(err));
+});
+
+const main = async () => {
+    try {
+        await pullData();
+        exec(HADOOP_CREATE_DIR);
+        exec(HADOOP_PUT_DOC);
+        process.exit(0);
+    } catch (err) {
+        console.error(err);
+        process.exit(1);
+    }
+};
