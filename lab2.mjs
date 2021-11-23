a6d5d65da242e54e76bf4644f0952aa5420584e0
KHYehor <khilchenko.yehor@gmail.com>
2021-11-24 00:00:29 +0200

    feat: add todo

--- lab2.mjs
+++ lab2.mjs
@@ -5,8 +5,11 @@ import util from 'util';
 
 const LINK_TO_PULL = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-20/master/data_tables/vaccine_data/global_data/time_series_covid19_vaccine_global.csv';
 const CSV_DATA_FILE = './vaccines.csv';
-const HADOOP_CREATE_DIR = 'hadoop fs -mkdir /covid19/';
-const HADOOP_PUT_DOC = `hadoop fs -put ${CSV_DATA_FILE} /covid19/vaccines`;
+
+const CLI_HADOOP_CREATE_DIR = 'hadoop fs -mkdir /covid19/';
+const CLI_HADOOP_PUT_CSV = `hadoop fs -put ${CSV_DATA_FILE} /covid19/vaccines`;
+const CLI_HADOOP_DELETE_CSV = `rm ${CSV_DATA_FILE}`;
+
 const TIMER_LABEL = 'Done in';
 
 const WriteStream = fs.createWriteStream(CSV_DATA_FILE);
@@ -26,8 +29,11 @@ const main = async () => {
     try {
         console.time(TIMER_LABEL);
         await pullData();
-        await exec(HADOOP_CREATE_DIR);
-        await exec(HADOOP_PUT_DOC);
+        await exec(CLI_HADOOP_CREATE_DIR);
+        await exec(CLI_HADOOP_PUT_CSV);
+        await exec(CLI_HADOOP_DELETE_CSV);
+        // TODO: script for creating table with fields
+        // Country_Region,Date,Doses_admin,People_partially_vaccinated,People_fully_vaccinated,Report_Date_String,UID,Province_State
         console.timeEnd(TIMER_LABEL);
         process.exit(0);
     } catch (err) {
